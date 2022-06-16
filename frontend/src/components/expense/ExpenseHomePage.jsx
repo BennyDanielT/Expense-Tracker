505eaf9865bd8caec618745dd8f61e9777b7a3c5
Abhishek Uppe <ab577089@dal.ca>
2022-06-16 17:50:31 -0300

    updated expense

--- frontend/src/components/expense/ExpenseHomePage.jsx
+++ frontend/src/components/expense/ExpenseHomePage.jsx
@@ -1,44 +1,28 @@
-import {useEffect, useState} from "react";
-import {getLocalStorage, routes} from "../../constants";
+import {useState} from "react";
+import {routes} from "../../constants";
 import {Button, Form} from "react-bootstrap";
 import {useHistory} from "react-router-dom";
-import Swal from "sweetalert2";
 import "./expense.css";
 import {Heading} from "../Heading/Heading";
 
 function ExpenseHomePage() {
-    const [groups, updateGroups] = useState([]);
-
-    useEffect(() => {
-        const groups = JSON.parse(getLocalStorage("groups"));
-        if (groups) {
-            updateGroups(groups);
-        }
-    }, []);
 
     const history = useHistory();
 
-    const onClickLink = (e, currentGroup) => {
-        e.preventDefault();
-        history.push(routes.viewGroup.path.split(":")[0] + currentGroup[0]);
-    }
+    const defaultList = [
+        {name: "Expense 1"},
+        {name: "Expense 2"},
+        {name: "Expense 3"}
+    ];
 
-    
-        const defaultList = [
-          { name: "Expense 1" },
-          { name: "Expense 2" },
-          { name: "Expense 3" }
-        ];
-    
 
     const [list, updateList] = useState(defaultList);
 
-  const handleRemoveItem = (e) => {
-   const name = e.target.getAttribute("name")
-    updateList(list.filter(item => item.name !== name));
-  };
-    
-     
+    const handleRemoveItem = (e) => {
+        const name = e.target.getAttribute("name")
+        updateList(list.filter(item => item.name !== name));
+    };
+
 
     return (
         <div className="groups-home">
@@ -46,37 +30,26 @@ function ExpenseHomePage() {
             <div className="current-groups m-4">
                 <div className="mb-4 mt-4"><b>Current Expenses</b></div>
                 <div className="mb-4 mt-3">
-                <Button variant="primary" type="click" onClick={() => history.push(routes.viewExpense.path)}>
-                    View Expenses
-                </Button>
+                    <Button variant="primary" type="click" onClick={() => history.push(routes.viewExpense.path)}>
+                        View Expenses
+                    </Button>
                 </div>
                 <div className="delete">
-                {list.map(item => {
-                    return (
-                        <>
+                    {list.map(item => {
+                        return (
+                            <>
                             <span name={item.name} onClick={handleRemoveItem}>
                              Delete Expense
                             </span>
-                            <span>{item.name}</span>
-                        </>
+                                <span>{item.name}</span>
+                            </>
                         );
-                        })}
-                    </div>
-                <div className="groups-list mb-4">
-                    {groups.length ? groups.map((group) => Object.entries(group).map((currentGroup) => {
-                        return (
-                            <div key={currentGroup[1].name} className="m-2">
-                                <a href={"#"} onClick={(e) => onClickLink(e, currentGroup)}>
-                                    {currentGroup[1].name}
-                                </a>
-                            </div>
-                        )
-                    })) : <div>Click to add an expense.</div>}
+                    })}
                 </div>
                 <Button variant="primary" type="click" onClick={() => history.push(routes.addExpense.path)}>
                     Add Expense
                 </Button>
-                
+
             </div>
         </div>
     )
