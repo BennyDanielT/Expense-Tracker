c9d2f91a3170ea7c93c2ef80f8e9369eb68d78be
Abhishek Uppe <ab577089@dal.ca>
2022-06-16 18:34:31 -0300

    updated footer

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,7 +5,6 @@ import 'react-pro-sidebar/dist/css/styles.css';
 import "react-datepicker/dist/react-datepicker.css";
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -17,4 +16,3 @@ root.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
