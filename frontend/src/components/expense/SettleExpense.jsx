882599d5e78948941167eeb360274f93c9c9a060
Abhishek Uppe <ab577089@dal.ca>
2022-06-16 17:44:52 -0300

    adde new task

--- frontend/src/components/expense/SettleExpense.jsx
+++ frontend/src/components/expense/SettleExpense.jsx
@@ -5,26 +5,26 @@ import Button from 'react-bootstrap/Button';
 import Swal from "sweetalert2";
 
 export default function SettleExpense() {
-  return (
-    <div style={{ display: 'block', 
-                  width: 700, 
-                  padding: 30 }}>
-      <h4>Settle the Expense</h4>
-      <Form>
-      <Form.Group>
-          <Form.Label>Enter the amount to be settled:</Form.Label>
-          <Form.Control type="text" 
-                        placeholder="Enter amount" />
-        </Form.Group>
-        
-        <Button variant="primary" type="button" onClick={()=>Swal.fire(
-            'Congratulations!',
-            'You are all settled up!',
-            'success'
-            )}>
-           Settle Up!
-        </Button>
-      </Form>
-    </div>
-  );
+    return (
+        <div style={{
+            display: 'block'
+        }}>
+            <h4>Settle the Expense</h4>
+            <Form>
+                <Form.Group>
+                    <Form.Label>Enter the amount to be settled:</Form.Label>
+                    <Form.Control type="text"
+                                  placeholder="Enter amount"/>
+                </Form.Group>
+
+                <Button variant="primary" type="button" onClick={() => Swal.fire(
+                    'Congratulations!',
+                    'You are all settled up!',
+                    'success'
+                )}>
+                    Settle Up!
+                </Button>
+            </Form>
+        </div>
+    );
 }
