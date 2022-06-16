a6b623554c01ad9d73c5706764ae2c5b74eb78d6
Abhishek Uppe <ab577089@dal.ca>
2022-06-16 13:19:45 -0300

    added sidebar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,7 +3,7 @@ import {Routing} from "./components/routing";
 
 function App() {
     return (
-        <div>
+        <div className="h-100 d-flex">
             <Routing/>
         </div>
     );
--- frontend/src/components/routing/index.js
+++ frontend/src/components/routing/index.js
@@ -1,101 +1,105 @@
-import { BrowserRouter, Route, Switch } from "react-router-dom";
-import { routes } from "../../constants";
-import { GroupHomePage } from "../group/GroupHomePage";
-import { CreateGroup } from "../group/CreateGroup";
+import {BrowserRouter, Route, Switch} from "react-router-dom";
+import {routes} from "../../constants";
+import {GroupHomePage} from "../group/GroupHomePage";
+import {CreateGroup} from "../group/CreateGroup";
 import RemindersGrid from "../PaymentReminders/RemindersGrid";
 import CreateReminder from "../PaymentReminders/CreateReminder";
-import { ViewGroup } from "../group/ViewGroup";
-import { EditGroup } from "../group/EditGroup";
-import { DeleteGroup } from "../group/DeleteGroup";
-import { ViewNotification } from "../notifications/ViewNotification";
-import { EmailNotification } from "../notifications/EmailNotification";
-import { NotificationSettings } from "../notifications/NotificationSettings";
+import {ViewGroup} from "../group/ViewGroup";
+import {EditGroup} from "../group/EditGroup";
+import {DeleteGroup} from "../group/DeleteGroup";
+import {ViewNotification} from "../notifications/ViewNotification";
+import {EmailNotification} from "../notifications/EmailNotification";
+import {NotificationSettings} from "../notifications/NotificationSettings";
 import Grid from "../ExportData/Grid";
-import { ViewCoupons } from "../CouponManagement/viewCoupons";
-import { ReedemCoupon } from "../CouponManagement/redeemCoupon";
+import {ViewCoupons} from "../CouponManagement/viewCoupons";
+import {ReedemCoupon} from "../CouponManagement/redeemCoupon";
 import Layout from "../CouponManagement/Layout/Layout";
 import NotFound from "../CouponManagement/notFound";
-import { CouponRedeemed } from "../CouponManagement/couponRedeemed";
-import { Login } from "../UserManagement/Login";
-import { Register } from "../UserManagement/Register";
-import { ForgotPassword } from "../UserManagement/ForgetPassword";
-import { PasswordChanged } from "../UserManagement/ChangePassword";
+import {CouponRedeemed} from "../CouponManagement/couponRedeemed";
+import {Login} from "../UserManagement/Login";
+import {Register} from "../UserManagement/Register";
+import {ForgotPassword} from "../UserManagement/ForgetPassword";
+import {PasswordChanged} from "../UserManagement/ChangePassword";
+import {SideBar} from "../SideBar";
+
 function Routing() {
-  return (
-    <BrowserRouter>
-      <Switch>
-        <Route exact path={routes.home.path}>
-          <div>Home</div>
-        </Route>
+    return (
+        <BrowserRouter>
+            <SideBar/>
+            <div className="main">
+                <Switch>
+                    <Route exact path={routes.home.path}>
+                        <div>Home</div>
+                    </Route>
 
-        <Route exact path={routes.viewCoupons.path}>
-          <Layout>
-            <ViewCoupons />
-          </Layout>
-        </Route>
-        <Route exact path={routes.notFound.path}>
-          <Layout>
-            <NotFound />
-          </Layout>
-        </Route>
-        <Route exact path={routes.redeemCoupon.path}>
-          <Layout>
-            <ReedemCoupon />
-          </Layout>
-        </Route>
-        <Route exact path={routes.couponRedeemed.path}>
-          <Layout>
-            <CouponRedeemed />
-          </Layout>
-        </Route>
+                    <Route exact path={routes.viewCoupons.path}>
+                        <Layout>
+                            <ViewCoupons/>
+                        </Layout>
+                    </Route>
+                    <Route exact path={routes.notFound.path}>
+                        <Layout>
+                            <NotFound/>
+                        </Layout>
+                    </Route>
+                    <Route exact path={routes.redeemCoupon.path}>
+                        <Layout>
+                            <ReedemCoupon/>
+                        </Layout>
+                    </Route>
+                    <Route exact path={routes.couponRedeemed.path}>
+                        <Layout>
+                            <CouponRedeemed/>
+                        </Layout>
+                    </Route>
 
-        <Route exact path={routes.group.path}>
-          <GroupHomePage />
-        </Route>
-        <Route exact path={routes.login.path}>
-          <Login />
-        </Route>
-        <Route exact path={routes.register.path}>
-          <Register />
-        </Route>
-        <Route exact path={routes.forgotPassword.path}>
-          <ForgotPassword />
-        </Route>
-        <Route exact path={routes.passwordChanged.path}>
-          <PasswordChanged />
-        </Route>
-        <Route exact path={routes.createGroup.path}>
-          <CreateGroup />
-        </Route>
-        <Route exact path={routes.viewGroup.path}>
-          <ViewGroup />
-        </Route>
-        <Route exact path={routes.editGroup.path}>
-          <EditGroup />
-        </Route>
-        <Route exact path={routes.deleteGroup.path}>
-          <DeleteGroup />
-        </Route>
-        <Route exact path={routes.viewNotification.path}>
-          <ViewNotification />
-        </Route>
-        <Route exact path={routes.emailNotification.path}>
-          <EmailNotification />
-        </Route>
-        <Route exact path={routes.notificationSettings.path}>
-          <NotificationSettings />
-        </Route>
-        <Route exact path={routes.reminders.path}>
-          <RemindersGrid />
-        </Route>
-        <Route exact path={routes.createReminder.path}>
-          <CreateReminder />
-        </Route>
+                    <Route exact path={routes.group.path}>
+                        <GroupHomePage/>
+                    </Route>
+                    <Route exact path={routes.login.path}>
+                        <Login/>
+                    </Route>
+                    <Route exact path={routes.register.path}>
+                        <Register/>
+                    </Route>
+                    <Route exact path={routes.forgotPassword.path}>
+                        <ForgotPassword/>
+                    </Route>
+                    <Route exact path={routes.passwordChanged.path}>
+                        <PasswordChanged/>
+                    </Route>
+                    <Route exact path={routes.createGroup.path}>
+                        <CreateGroup/>
+                    </Route>
+                    <Route exact path={routes.viewGroup.path}>
+                        <ViewGroup/>
+                    </Route>
+                    <Route exact path={routes.editGroup.path}>
+                        <EditGroup/>
+                    </Route>
+                    <Route exact path={routes.deleteGroup.path}>
+                        <DeleteGroup/>
+                    </Route>
+                    <Route exact path={routes.viewNotification.path}>
+                        <ViewNotification/>
+                    </Route>
+                    <Route exact path={routes.emailNotification.path}>
+                        <EmailNotification/>
+                    </Route>
+                    <Route exact path={routes.notificationSettings.path}>
+                        <NotificationSettings/>
+                    </Route>
+                    <Route exact path={routes.reminders.path}>
+                        <RemindersGrid/>
+                    </Route>
+                    <Route exact path={routes.createReminder.path}>
+                        <CreateReminder/>
+                    </Route>
 
-        <Route exact path={routes.exportGrid.path}>
-          <Grid />
-        </Route>
-        {/* <Route exact path={routes.createSnapshot.path}>
+                    <Route exact path={routes.exportGrid.path}>
+                        <Grid/>
+                    </Route>
+                    {/* <Route exact path={routes.createSnapshot.path}>
                     <CreateSnapshot/>
                 </Route>
                 <Route exact path={routes.viewSnapshot.path}>
@@ -113,9 +117,10 @@ function Routing() {
                 <Route exact path={routes.paymentHistory.path}>
                     <PaymentHistory/>
                 </Route> */}
-      </Switch>
-    </BrowserRouter>
-  );
+                </Switch>
+            </div>
+        </BrowserRouter>
+    );
 }
 
-export { Routing };
+export {Routing};
