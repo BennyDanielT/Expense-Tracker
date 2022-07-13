fd1339988e8f7d05bb5242519f339687fb4b81dc
Abhishek Uppe <ab577089@dal.ca>
2022-07-13 20:19:15 -0300

    updated code for groups

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,8 +1,12 @@
 import express from "express";
-import {createGroup} from "../controllers/group.js";
+import {createGroup, deleteGroup, updateGroup, viewGroup, viewGroups} from "../controllers/group.js";
 
 const router = express.Router();
 
 router.post("/create-group", createGroup);
+router.put("/update-group/:id", updateGroup);
+router.delete("/delete-group/:id", deleteGroup);
+router.get("/view-groups", viewGroups);
+router.get("/view-group/:id", viewGroup);
 
 export {router}
