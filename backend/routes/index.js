0d1fc0fa59a32a2edc601bd6dc9c255b4b4c0f96
Ayush Verma <ay322266@dal.ca>
2022-07-14 15:38:07 -0300

    Added user auth

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,6 +1,14 @@
 import express from "express";
-import {createGroup, deleteGroup, updateGroup, viewGroup, viewGroups} from "../controllers/group.js";
+import {
+  createGroup,
+  deleteGroup,
+  updateGroup,
+  viewGroup,
+  viewGroups,
+} from "../controllers/group.js";
 
+import { getCoupons } from "../controllers/coupons.js";
+import { userSignup, userSignin } from "../controllers/userManagement.js";
 const router = express.Router();
 
 router.post("/create-group", createGroup);
@@ -8,5 +16,8 @@ router.put("/update-group/:id", updateGroup);
 router.delete("/delete-group/:id", deleteGroup);
 router.get("/view-groups", viewGroups);
 router.get("/view-group/:id", viewGroup);
-
-export {router}
+router.post("/user-signup", userSignup);
+// routing for the coupon management
+router.get("/get-coupons", getCoupons);
+router.post("/sign-in", userSignin);
+export { router };
