b88a5cb6c6f32f10067f8f509fb1fa54759ddc53
devarshi.vyas <dv459527@dal.ca>
2022-07-14 09:32:42 -0300

    Added tags CRUD to the backend

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,12 +1,35 @@
 import express from "express";
-import {createGroup, deleteGroup, updateGroup, viewGroup, viewGroups} from "../controllers/group.js";
+
+import {
+  createGroup,
+  deleteGroup,
+  updateGroup,
+  viewGroup,
+  viewGroups,
+} from "../controllers/group.js";
+
+import {
+  createTag,
+  deleteTag,
+  updateTag,
+  viewTag,
+  viewTags,
+} from "../controllers/tags.js";
 
 const router = express.Router();
 
+// group routes
 router.post("/create-group", createGroup);
 router.put("/update-group/:id", updateGroup);
 router.delete("/delete-group/:id", deleteGroup);
 router.get("/view-groups", viewGroups);
 router.get("/view-group/:id", viewGroup);
 
-export {router}
+// tags routes
+router.post("/create-tag", createTag);
+router.put("/update-tag/:id", updateTag);
+router.delete("/delete-tag/:id", deleteTag);
+router.get("/view-tags", viewTags);
+router.get("/view-tag/:id", viewTag);
+
+export { router };
