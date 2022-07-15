c559f8558da3579cc95e5e1e471f86d7fbeeccfc
Vatsal Yadav <vatsal.yadav@dal.ca>
2022-07-15 19:12:00 -0300

    Schedule emails for Payment Reminders

--- backend/controllers/reminders.js
+++ backend/controllers/reminders.js
@@ -1,9 +1,11 @@
 import {supabase} from "../models/index.js";
 import {errorCodeResponses, isFieldAbsent} from "../utils.js";
+import nodemailer from "nodemailer";
+import schedule from "node-schedule";
 
 export const createReminder = async (request, response) => {
 
-    const {name, amount, user_id, desc, date} = request.body;
+    const {name, amount, user_id, desc, date, email} = request.body;
 
     try {
         const fields = [
@@ -45,6 +47,38 @@ export const createReminder = async (request, response) => {
             return response.status(400).send(error);
         }
 
+        console.log("user", email)
+
+        // e-mail message options
+        let mailOptions = {
+            from: 'tripmanagementasdc@gmail.com',
+            to: email,
+            subject: 'Payment Reminder for '+name,
+            text: 'Payment Reminder \nAmount: '+amount+'$ \nDetails: '+name+' - '+desc
+        };
+
+        // e-mail transport configuration
+        let transporter = nodemailer.createTransport({
+            host: 'smtp.gmail.com',
+            port: 465,
+            secure: true,
+            auth: {
+                user: 'tripmanagementasdc@gmail.com',
+                pass: 'ythscondtmxkatkj'
+            }
+        });
+
+        schedule.scheduleJob(data[0].id.toString(),new Date(date), function(){
+            console.log('Sending mail');
+            transporter.sendMail(mailOptions, function(error, info){
+                if (error) {
+                    console.log(error);
+                } else {
+                    console.log('Email sent: ' + info.response);
+                }
+            });
+        });
+
         return response.send({success: data});
     } catch (e) {
         return response.status(500).send(errorCodeResponses["500"]);
@@ -54,7 +88,7 @@ export const createReminder = async (request, response) => {
 
 export const updateReminder = async (request, response) => {
 
-    const {id, name, amount, user_id, desc, date} = request.body;
+    const {id, name, amount, user_id, desc, date, email} = request.body;
 
     try {
         const fields = [
@@ -102,6 +136,43 @@ export const updateReminder = async (request, response) => {
             return response.status(400).send(error);
         }
 
+        let current_job = schedule.scheduledJobs[id.toString()];
+        current_job.cancel();
+
+        console.log("user", email)
+
+        // e-mail message options
+        let mailOptions = {
+            from: 'tripmanagementasdc@gmail.com',
+            to: email,
+            subject: 'Payment Reminder for '+name,
+            text: 'Payment Reminder \nAmount: '+amount+'$ \nDetails: '+name+' - '+desc
+        };
+
+        // e-mail transport configuration
+        let transporter = nodemailer.createTransport({
+            host: 'smtp.gmail.com',
+            port: 465,
+            secure: true,
+            auth: {
+                user: 'tripmanagementasdc@gmail.com',
+                pass: 'ythscondtmxkatkj'
+            }
+        });
+
+        schedule.scheduleJob(data[0].id.toString(),new Date(date), function(){
+            console.log('Sending mail at '+(new Date()) );
+            transporter.sendMail(mailOptions, function(error, info){
+                if (error) {
+                    console.log(error);
+                } else {
+                    console.log('Email sent: ' + info.response);
+                }
+            });
+        });
+
+
+
         return response.send({success: data});
     } catch (e) {
         return response.status(500).send(errorCodeResponses["500"]);
@@ -118,6 +189,10 @@ export const deleteReminder = async (request, response) => {
         if (error) {
             return response.status(400).send(error);
         }
+
+        let current_job = schedule.scheduledJobs[id.toString()];
+        current_job.cancel();
+
         return response.send({success: data});
 
     } catch (e) {
