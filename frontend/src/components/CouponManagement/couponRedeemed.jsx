fb645bb9637ebe830e7603f0f7d53d01e201ed60
Ayush Verma <ay322266@dal.ca>
2022-07-19 18:36:04 -0300

    Added maps and coupon locations for the merchants

--- frontend/src/components/CouponManagement/couponRedeemed.jsx
+++ frontend/src/components/CouponManagement/couponRedeemed.jsx
@@ -5,20 +5,30 @@ import Meta from "./Helpers/meta";
 import { Container, Card, Table } from "react-bootstrap";
 import "../../css/coupon.css";
 import { useParams } from "react-router-dom";
-
+import {
+  Row,
+  Alert,
+  Carousel,
+  Col,
+  Placeholder,
+  Button,
+  ButtonGroup,
+  Accordion,
+} from "react-bootstrap";
 function CouponRedeemed() {
   let { id } = useParams();
-  console.log(id);
-  const [review, setReview] = React.useState(null);
-  React.useEffect(() => {
-    fetch("localhost:3001/api/get-location/" + id)
+  const [locations, setLocation] = useState([]);
+  const fetchData = async () => {
+    fetch("http://localhost:3001/api/get-location/" + id)
       .then((results) => results.json())
       .then((data) => {
-        console.log(data);
-        setReview(data);
+        setLocation(data);
       });
-  }, []); // <-- Have to pass in [] here!
-  // page content
+  };
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   const pageDescription = ". ";
 
   return (
@@ -28,6 +38,7 @@ function CouponRedeemed() {
           <Meta />
           <Header description={pageDescription} />
         </div>
+
         <div class="vh-10 d-flex justify-content-center ">
           <Table striped bordered hover>
             <thead>
@@ -39,18 +50,14 @@ function CouponRedeemed() {
               </tr>
             </thead>
             <tbody>
-              <tr>
-                <td>1</td>
-                <td>Subway #93</td>
-                <td>6460 LeMerchant Aveneue</td>
-                <td>Subway-{Math.random().toString(36).slice(2, 7)}</td>
-              </tr>
-              <tr>
-                <td>2</td>
-                <td>Subway #431</td>
-                <td>3433 Chainlake Dvec</td>
-                <td>Subway-{Math.random().toString(36).slice(2, 7)}</td>
-              </tr>
+              {locations.map((_, idx) => (
+                <tr>
+                  <td>{idx}</td>
+                  <td>{_.merchant_name}</td>
+                  <td>{_.address}</td>
+                  <td>Subway-{Math.random().toString(36).slice(2, 7)}</td>
+                </tr>
+              ))}
             </tbody>
           </Table>
         </div>
@@ -68,7 +75,7 @@ function CouponRedeemed() {
           <br></br>
 
           <iframe
-            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3966.521260322283!2d106.8195613507864!3d-6.194741395493371!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e69f5390917b759%3A0x6b45e67356080477!2sPT%20Kulkul%20Teknologi%20Internasional!5e0!3m2!1sen!2sid!4v1601138221085!5m2!1sen!2sid"
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d90835.04760642834!2d-63.63289745000001!3d44.64618905!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4b5a211407dbfac1%3A0x666be3a6438b2ddc!2sHalifax%2C%20NS!5e0!3m2!1sen!2sca!4v1658266465910!5m2!1sen!2sca"
             width="600"
             height="450"
             frameBorder="0"
